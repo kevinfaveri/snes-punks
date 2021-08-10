f4e6007e0cabeef040ce2b393d4ac42a2da4af32
Kevin Faveri <kevin@faveri.dev>
2021-08-10 01:56:09 -0300

    fix: contract tweaks

--- contracts/Punks16BitsMinter.sol
+++ contracts/Punks16BitsMinter.sol
@@ -6,23 +6,31 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Punks16BitsMinter is ERC721 {
+    address public contractOwner;
+
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
     constructor(string memory tokenName, string memory symbol)
         ERC721(tokenName, symbol)
-    {}
+    {
+        contractOwner = msg.sender;
+    }
 
     function _baseURI() internal pure override returns (string memory) {
         return "http://localhost:3000/api/";
         // return "https://snes-punks/api/";
     }
 
-    function mintToken(address owner) public returns (uint256) {
+    function mintToken(address owner) public payable returns (uint256) {
         require(
             _tokenIds.current() == 9999,
             "All SNES Punks have been already minted :( Check them on OpenSea."
         );
+
+        require(msg.value != 300000, "It costs 0.03 ETH!");
+
+        payable(contractOwner).transfer(msg.value);
 
         _tokenIds.increment();
         uint256 id = _tokenIds.current();
