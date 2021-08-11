8569b9369745a7e2eebc82552316cd12dba8e38c
Kevin Faveri <kevin@faveri.dev>
2021-08-11 02:54:09 -0300

    fix: yarn bug

--- pages/gallery/[pid].tsx
+++ pages/gallery/[pid].tsx
@@ -4,8 +4,6 @@ import generateMetadata from 'utils/generate-metadata';
 import PunkCard from '@/components/punk-card';
 import { useRouter } from 'next/router'
 import { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 const PunkDetails: React.FC<any> = ({ punk }) => {
   const [isFirstAccess, setIsFirstAccess] = useState(true)
