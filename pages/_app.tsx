18c9a74b71da8e54524e5cf9a5d7640e56fce4f9
Kevin Faveri <kevin@faveri.dev>
2021-08-12 02:00:55 -0300

    fix: add switcher to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,16 @@ import "./global.css"
 import Layout from '@/components/layout';
 import { Toaster } from 'react-hot-toast';
 
+if (process.browser) {
+  if (window && window.ethereum !== undefined) {
+    window.ethereum.request({
+      method: "wallet_switchEthereumChain",
+      params: [{ chainId: '0x4' }],
+    });
+  }
+}
 function MyApp({ Component, pageProps }: any) {
+
   return <Layout>
     <Component {...pageProps} />
     <Toaster />
