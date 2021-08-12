bfcf71fdbe622b23a1f11e6a26453048a0c0d9a5
Kevin Faveri <kevin@faveri.dev>
2021-08-12 14:42:07 -0300

    fix: remove disposals

--- pages/api/punks/[pid].ts
+++ pages/api/punks/[pid].ts
@@ -4,7 +4,6 @@ import { db, punksDb } from "utils/pool"
 export default async function getSpecificPunk(req: any, res: any) {
   const { pid } = req.query
   const punk: any = await punksDb(db).findOne({ id: pid }) || {}
-  await db.dispose()
   const data = await generateMetadata(punk)
 
   res.status(200).json(data)
