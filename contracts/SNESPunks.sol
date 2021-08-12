cd13a1d3c577d04993d1fbc7ae4a3ba16c22c25b
Kevin Faveri <kevin@faveri.dev>
2021-08-12 13:45:42 -0300

    fix: remove console.log from contract

--- contracts/SNESPunks.sol
+++ contracts/SNESPunks.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "hardhat/console.sol";
+// import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
