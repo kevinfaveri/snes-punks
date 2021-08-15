4cb1681fb122885f56d1dd631012901f247c20d6
Kevin Faveri <kevin@faveri.dev>
2021-08-15 14:05:29 -0300

    fix: remove unused keys

--- utils/spaces.ts
+++ utils/spaces.ts
@@ -1,13 +1,3 @@
-const AWS = require('aws-sdk');
-
-const spacesEndpoint = new AWS.Endpoint('nyc3.digitaloceanspaces.com');
-
-const s3 = new AWS.S3({
-  endpoint: spacesEndpoint,
-  accessKeyId: 'XQORQLFBSSJR2IUHSZPC',
-  secretAccessKey: 'Wie6kypyEJDdZxitMp0INMQ2K5Sw1KMUoH8UjI5AA48'
-});
-
 const spacesBucket = 'snes-16bit-punks'
 
 export const getImageUrl = async (id) => `https://${spacesBucket}.nyc3.cdn.digitaloceanspaces.com/${id}.png`
